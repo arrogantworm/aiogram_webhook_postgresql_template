1de1513d6d62c161f868708ed75f66c044d56948
DiLDDoS <ctulhuthemonster@gmail.com>
2022-12-06 02:15:26 +0300

    insert fix

--- core/utils/dbconnect.py
+++ core/utils/dbconnect.py
@@ -6,6 +6,6 @@ class Request:
         self.connector = connector
 
     async def new_user(self, user_id, username):
-        query = """INSERT INTO UserInfo (user_id, username) VALUES (%i, '%s')
-                ON CONFLICT (user_id) DO UPDATE SET username='%s'""" % (user_id, username, username)
-        await self.connector.execute(query)
+        query = """INSERT INTO UserInfo (user_id, username) VALUES (?, ?)
+                ON CONFLICT (user_id) DO UPDATE SET username=?"""
+        await self.connector.execute(query, (user_id, username, username))
